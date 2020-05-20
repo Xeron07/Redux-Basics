a0515101b2f9036bbdd866cfbd3afadce71aefdb
xeron07 <ni.xeron07@gmail.com>
2020-05-20 19:38:26 +0600

    some note about spread object copy

--- Functional Programming with JS/immutability.js
+++ Functional Programming with JS/immutability.js
@@ -1,5 +1,6 @@
 /**
  * spread operator object use (@implements) shallow copy for 
- * nested object. So the more  nested object the 
+ * nested object.We have to do deep copy to prevent it.
+ *  So the more  nested object the 
  * more verbose the object will be
  */
