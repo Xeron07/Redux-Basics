ef840ca09241cd762c5f9c8eca59cf6caf9fecdd
xeron07 <ni.xeron07@gmail.com>
2020-05-21 05:32:21 +0600

    creating reducer

---
+++ src/reducer.js
@@ -0,0 +1,28 @@
+
+/**
+ * Action structure
+ * {
+ *  type: "string",
+ *  payload:{
+ *    id: number,
+ *    description: "string"
+ * }
+ * }
+ */
+
+
+let lastId=0;
+
+//initialy redux will call reducer with undefined state
+function reducer(state=[],action){
+
+    switch(action.type){
+
+        case "bugAdded":
+            return [...state,{id:++lastId,description:action.payload.description,resolved:false}];
+        case "bugRemoved":
+            return state.filter(bug=>bug.id !== action.payload.id);
+        default: return state;
+    }
+
+}
