5f3b440678b873c4e22117727e7866643a8a343d
xeron07 <ni.xeron07@gmail.com>
2020-05-21 05:57:26 +0600

    adding actions from action js in reducer

--- src/reducer.js
+++ src/reducer.js
@@ -10,7 +10,7 @@
  * }
  */
 
-
+import * as actions from "./action";
 let lastId=0;
 
 //initially redux will call reducer with undefined state
@@ -18,9 +18,9 @@ export default function reducer(state=[],action){
 
     switch(action.type){
 
-        case "bugAdded":
+        case actions.BUG_ADDED:
             return [...state,{id:++lastId,description:action.payload.description,resolved:false}];
-        case "bugRemoved":
+        case actions.BUG_REMOVED:
             return state.filter(bug=>bug.id !== action.payload.id);
         default: return state;
     }
