5ab2ba707fa002392d89d7ee1912245f3e8e2818
xeron07 <ni.xeron07@gmail.com>
2020-05-20 18:17:34 +0600

    curring fucntion example with fu8nction

--- Functional Programming with JS/curring.js
+++ Functional Programming with JS/curring.js
@@ -4,5 +4,24 @@
  * and convert to a single argument function
  */
 
-//importing package
-import {compose, pipe} from 'lodash/fp'; 
+
+ //Example
+
+ function add(a,b){
+     return a+b;
+ }
+
+ //calling add function
+ add(1,2);
+
+ //after curring
+ function addC(a){
+     return fucntion(b){
+         return a+b;
+     }
+ }
+
+ //this curry function takes two parantesis
+ // instead of 2 arguments
+//calling curring function
+ addC(1)(2)//replace of add(1,2)
