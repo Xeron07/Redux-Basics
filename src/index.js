a2e817b0a08dfca3685dee6177a55c7092c4e31f
xeron07 <ni.xeron07@gmail.com>
2020-05-21 05:54:46 +0600

    adding store to app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+import store from "./store";
+
+//subscription to store
+const unsubscribe =store.subscribe(()=>{
+    console.log("store changed");
+});
+
+store.dispatch({
+    type:"bugAdded",
+    paylaod :{
+        description :"bug1"
+    }
+});
+
+//unsubscribinng the store subscription to prevent memory leak
+unsubscribe();
+
+store.dispatch({
+    type:"bugRemoved",
+    paylaod :{
+        id:1
+    }
+});
+
+console.log(store);
 console.log("Hello World!");
