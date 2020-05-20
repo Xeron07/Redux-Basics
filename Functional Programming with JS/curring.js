a535371920675f63604d905c86c4e6d3fe3d3850
xeron07 <ni.xeron07@gmail.com>
2020-05-20 18:13:32 +0600

    lodash example and curring starting

---
+++ Functional Programming with JS/curring.js
@@ -0,0 +1,8 @@
+/**
+ * Curring is a technique that allow
+ * us to take a function of N arguments
+ * and convert to a single argument function
+ */
+
+//importing package
+import {compose, pipe} from 'lodash/fp'; 
---
+++ Functional Programming with JS/lodash.js
@@ -0,0 +1,35 @@
+/**
+ * example of lodash for composing & piping
+ */
+
+//importing package
+import {compose, pipe} from 'lodash/fp'; 
+
+
+let input = "  Javascript  ";
+
+//Non Functional syntex
+let output= "<div>"+input+"</div>";
+
+//Functional syntex
+const trim = str=>str.trim();
+const wrapInDiv=str=>`<div>${trim}</div>`;
+const toLowerCase=str=>str.toLowerCase();
+
+//This is an example of Function Composition
+//old tech
+const result=wrapInDiv(toLowerCase(trim(input)));
+
+//using compose
+//in compose function order of operation
+//is from right to left
+//compose is a higher order function
+const transform=compose(wrapInDiv,toLowerCase,trim)//only passing function as argument
+transform(input);
+
+
+//using pipe function
+//in pipe order of operation is
+//left to right
+//pipe() is also a higher order function
+const transformFomPipe=pipe(trim,toLowerCase,wrapInDiv);
