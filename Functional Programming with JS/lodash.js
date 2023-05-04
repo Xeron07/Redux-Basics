f946bc76ea61f6c5432ef80df1a57ad8c1d00239
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2023-05-04 17:39:36 +0600

    update | redux with typescript

--- Functional Programming with JS/lodash.js
+++ Functional Programming with JS/lodash.js
@@ -3,39 +3,36 @@
  */
 
 //importing package
-import {compose, pipe} from 'lodash/fp'; 
-
+import { compose, pipe } from "lodash/fp";
 
 let input = "  Javascript  ";
 
 //Non Functional syntex
-let output= "<div>"+input+"</div>";
+let output = "<div>" + input + "</div>";
 
 //Functional syntex
-const trim = str=>str.trim();
-const wrapInDiv=str=>`<div>${trim}</div>`;
-const toLowerCase=str=>str.toLowerCase();
+const trim = (str) => str.trim();
+const wrapInDiv = (str) => `<div>${trim}</div>`;
+const toLowerCase = (str) => str.toLowerCase();
 
 //This is an example of Function Composition
 //old tech
-const result=wrapInDiv(toLowerCase(trim(input)));
+const result = wrapInDiv(toLowerCase(trim(input)));
 
 //using compose
 //in compose function order of operation
 //is from right to left
 //compose is a higher order function
-const transform=compose(wrapInDiv,toLowerCase,trim)//only passing function as argument
+const transform = compose(wrapInDiv, toLowerCase, trim); //only passing function as argument
 transform(input);
-
 
 //using pipe function
 //in pipe order of operation is
 //left to right
 //pipe() is also a higher order function
-const transformFomPipe=pipe(trim,toLowerCase,wrapInDiv);
+const transformFomPipe = pipe(trim, toLowerCase, wrapInDiv);
 
-
- //using currying technique 
-const wrap=type=>str=>`<${type}>${trim}</${type}>`;
-const transformWithCurry=pipe(trim,toLowerCase,wrap("type"));//only passing function as argument
+//using currying technique
+const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
+const transformWithCurry = pipe(trim, toLowerCase, wrap("type")); //only passing function as argument
 transformWithCurry(input);
